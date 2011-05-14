cddc946b9dda9494d3d6d0096300aae4f3b54969
JoostZ68 <joostZ68@gmail.com>
2011-05-14 10:23:28 +0000

    Some administrative changes

---
+++ WeatherLoggerLib/WS4000Device.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UsbLibrary;
+
+namespace WeatherLoggerLib
+{
+    public class WS4000Device : HIDDevice
+    {
+    }
+}
