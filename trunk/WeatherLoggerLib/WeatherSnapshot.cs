6f8826d185acb2b65030eb9ae1f660e86b06a125
joostZ68@gmail.com <joostZ68@gmail.com@a956d446-b5a4-c06e-85b6-5d4b721c355d>
2011-06-02 10:07:08 +0000

    Created Database and associated EDM

--- trunk/WeatherLoggerLib/WeatherSnapshot.cs
+++ trunk/WeatherLoggerLib/WeatherSnapshot.cs
@@ -5,31 +5,34 @@ using System.Text;
 
 namespace WeatherLoggerLib
 {
-    public class WeatherSnapshot
+    public partial class WeatherSnapshot
     {
         // Offsets in the buffer
         const int INTERVAL = 0;
         const int INTERNAL_HUMIDTY = 1;
 
-        public DateTime Date { 
-            get; 
-            internal set;
-        }
+        //public DateTime Date { 
+        //    get; 
+        //    internal set;
+        //}
 
-        public int InternalHumidity { 
-            get; 
-            private set; 
-        }
+        //public int InternalHumidity { 
+        //    get; 
+        //    private set; 
+        //}
 
-        public int Interval { 
-            get; 
-            private set; 
-        }
+        //public int Interval { 
+        //    get; 
+        //    private set; 
+        //}
 
         public WeatherSnapshot(byte[] buffer, int offset)
         {
             Interval = buffer[INTERVAL + offset];
-            InternalHumidity = buffer[INTERNAL_HUMIDTY + offset];
+            IndoorHumidity = buffer[INTERNAL_HUMIDTY + offset];
         }
+
+        public WeatherSnapshot()
+        { }
     }
 }
