e64a2ed3d3e38187146d95bc6a02be92f59f03c2
joostZ68@gmail.com <joostZ68@gmail.com@a956d446-b5a4-c06e-85b6-5d4b721c355d>
2011-06-04 09:25:58 +0000

    Created WPF application and made it work with USB Library

---
+++ trunk/WeatherLogger/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+
+using WeatherLoggerLib;
+using UsbLibrary;
+
+namespace WeatherLogger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WS4000HidPort port;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // do this to get the HWND and set up the window hook  
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            HwndSource.FromHwnd(helper.Handle).AddHook(HwndSourceHookHandler);
+            port = new WS4000HidPort();
+            port.RegisterHandle(helper.Handle);
+        }
+
+        private IntPtr HwndSourceHookHandler(IntPtr hwnd, int msg, IntPtr wParam,
+            IntPtr lParam, ref bool handled)
+        {
+            Message m = new Message();
+            m.HWnd = hwnd;
+            m.Msg = msg;
+            m.WParam = wParam;
+            m.LParam = lParam;
+
+            port.ParseMessages(ref m);
+
+            return IntPtr.Zero;  
+        }
+    }
+}
