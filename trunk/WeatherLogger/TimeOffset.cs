2e0cb3abf907607a429d9adfc3f8bb4cb4cd384c
joostZ68@gmail.com <joostZ68@gmail.com@a956d446-b5a4-c06e-85b6-5d4b721c355d>
2011-06-05 13:30:52 +0000

    First databindings with DB

---
+++ trunk/WeatherLogger/TimeOffset.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeatherLogger
+{
+    public class TimeOffset
+    {
+        public TimeOffset()
+        {
+            Interval = new TimeSpan();
+            Name = "";
+        }
+
+        public TimeOffset(TimeSpan interval, String name)
+        {
+            Interval = interval;
+            Name = name;
+        }
+        public TimeSpan Interval { get; set; }
+        public String Name { get; set; }
+        public override String ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class TimeOffsets
+    {
+        public TimeOffsets()
+        {
+        }
+
+        public TimeOffset[] list = new TimeOffset[] {
+                                new TimeOffset(new TimeSpan(1, 0, 0, 0), "Day"),
+                                new TimeOffset(new TimeSpan(7, 0, 0, 0), "Week"),
+                                new TimeOffset(new TimeSpan(30, 0, 0, 0), "Month"),
+                                new TimeOffset(new TimeSpan(365, 0, 0, 0), "Year"),
+                            };
+
+
+       
+    }
+}
