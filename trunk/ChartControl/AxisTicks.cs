fd7eb573d30827158e5fd79481d8b0d529758c8d
joostZ68@gmail.com <joostZ68@gmail.com@a956d446-b5a4-c06e-85b6-5d4b721c355d>
2011-07-03 08:50:11 +0000

    Defined AxisTicks

---
+++ trunk/ChartControl/AxisTicks.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ChartControl
+{
+    class AxisTicks : Shape
+    {
+        protected double XSize { get; set; }
+        protected double YSize { get; set; }
+        private Axis _axis;
+
+        public Axis Axis
+        {
+            get { return _axis; }
+            set { _axis = value; }
+        }
+
+        private void UpdateUI()
+        {
+            rectGeometry = new RectangleGeometry(new Rect(new Size(XSize, YSize / 2)));
+        }
+
+        private RectangleGeometry rectGeometry = new RectangleGeometry();
+        protected override Geometry DefiningGeometry
+        {
+            get { return rectGeometry; }
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            XSize = availableSize.Width;
+            YSize = availableSize.Height;
+            UpdateUI();
+            return base.MeasureOverride(availableSize);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            XSize = finalSize.Width;
+            YSize = finalSize.Height;
+            UpdateUI();
+            return base.ArrangeOverride(finalSize);
+        }
+    }
+}
---
+++ trunk/ChartControl/Ticks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChartControl
+{
+    public class Ticks
+    {
+        public double Max { get; set; }
+
+        public double Min { get; set; }
+    }
+}
